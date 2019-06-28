6aff03bceac0a869114d19d52c80ead628ca36ae
unknown <jupiter85838@yahoo.com>
2019-06-27 20:55:48 -0400

    sharing data from admin to shop

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require("path")
-const routeDir=require("../util/path")
+const routeDir = require("../util/path")
 const router = express.Router()
+const products = []
 router.get("/add-product", (req, res, next) => {
 
     res.sendfile(path.join(routeDir, "views", "add-product.html"))
@@ -9,7 +10,10 @@ router.get("/add-product", (req, res, next) => {
     //   next()
 })
 router.post("/add-product", (req, res, next) => {
-    console.log(req.body)
+   // console.log(req.body)
+    products.push({ title: req.body.title })
     res.redirect('/');
 })
-module.exports = router
+//module.exports = router
+exports.routes = router
+exports.products = products
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require("path")
-const routeDir=require("../util/path")
+const routeDir = require("../util/path")
+const adminData = require("./admin")
 const route = express.Router()
 route.get("/", (req, res, next) => {
+    console.log("adminData:", adminData.products)
     res.sendFile(path.join(routeDir, "views", "shop.html"))
     // res.send("<h1>hello from express</h1>")
     //   next()
