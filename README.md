321cbefdce21cf10e2fbec8eb7446fdd719ecb09
unknown <jupiter85838@yahoo.com>
2019-06-28 13:42:50 -0400

    adding pug engine

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # project
+# Templating Engine
+ * Engines
+    * Pug
+      * app.set("view engine", "pug")
+      * app.set("views", "views")
+    * EJS
+    * express-handlebars
+
+     
+
--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,9 @@ const routeDir = require("../util/path")
 const adminData = require("./admin")
 const route = express.Router()
 route.get("/", (req, res, next) => {
-    console.log("adminData:", adminData.products)
-    res.sendFile(path.join(routeDir, "views", "shop.html"))
+     const products=adminData.products
+     res.render("shop",{prods:products,docTitle:"shop"})
+    // res.sendFile(path.join(routeDir, "views", "shop.html"))
     // res.send("<h1>hello from express</h1>")
     //   next()
 })
