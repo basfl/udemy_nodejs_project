6300d9d0283561a017b954def93291525977f048
unknown <jupiter85838@yahoo.com>
2019-07-07 11:42:02 -0400

    view engines

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express')
 const path = require("path")
+const bodyParser = require('body-parser')
+//const expressHbs = require('express-handlebars');
 
+
+
+const app = express()
+
+// app.engine(
+//     'hbs',
+//     expressHbs({
+//       layoutsDir: 'views/layouts/',
+//       defaultLayout: 'main-layout',
+//       extname: 'hbs'
+//     })
+//   );
+app.set('view engine', 'ejs');
+//app.set("view engine", "pug")
+app.set("views", "views")
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-const bodyParser = require('body-parser')
-const app = express()
-//app.use((req, res, next) => {
-//    console.log("in the express use")
-//    next()
-//})
-app.set("view engine", "pug")
-app.set("views", "views")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 app.use("/admin", adminData.routes)
 app.use(shopRoutes)
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
-    // res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: "/" });
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 })
 
 
 //const server = http.createServer(app)
 app.listen(3000);
+
+
+
--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,16 @@ const routeDir = require("../util/path")
 const adminData = require("./admin")
 const route = express.Router()
 route.get("/", (req, res, next) => {
-    const products = adminData.products
-    res.render("shop", { prods: products, pageTitle: "shop", path: "/" })
+    const products = adminData.products;
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  //  res.render("shop", { prods: products, pageTitle: "shop", path: "/" })
     // res.sendFile(path.join(routeDir, "views", "shop.html"))
     // res.send("<h1>hello from express</h1>")
     //   next()
