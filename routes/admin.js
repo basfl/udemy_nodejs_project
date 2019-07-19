e3cb07551f333437884b31038a84d2fa275dec48
unknown <jupiter85838@yahoo.com>
2019-07-19 16:48:30 -0400

    enhancment

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require("path")
 //const routeDir = require("../util/path")
-const productsController = require('../controllers/products')
+const adminController = require('../controllers/admin')
 const router = express.Router()
-
-router.get("/add-product", productsController.getAddProduct)
-router.post("/add-product", productsController.postAddProduct)
+// /admin/add-product => GET
+router.get("/add-product", adminController.getAddProduct)
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+// /admin/add-product => POST
+router.post("/add-product",adminController.postAddProduct)
 module.exports = router
 
 //exports.routes = router
