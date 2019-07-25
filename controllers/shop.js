43cbb7e540a970034c610fd512a891cf9968237b
unknown <jupiter85838@yahoo.com>
2019-07-25 17:25:06 -0400

    adding details and moving unnecessary files

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,12 +12,17 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId
-  Product.findById(prodId,product=>{
-    console.log(product)
+  Product.findById(prodId, product => {
+    //  console.log(product)
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   })
-  console.log("product is is ", prodId)
+  // console.log("product is is ", prodId)
 
-  res.redirect("/")
+  // res.redirect("/")
 
 };
 
