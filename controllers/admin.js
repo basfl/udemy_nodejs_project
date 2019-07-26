a32f2197d553d420be2ce8841c3f1d93514f6a63
unknown <jupiter85838@yahoo.com>
2019-07-26 10:48:32 -0400

    edit the product

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,12 +11,30 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  console.log("*******",updatedTitle)
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice 
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
+
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
