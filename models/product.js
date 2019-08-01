b1cfacd2ca9d4a1b4e586e3e246899e82447b326
unknown <jupiter85838@yahoo.com>
2019-07-31 20:14:12 -0400

    inserting value

--- models/product.js
+++ models/product.js
@@ -13,18 +13,20 @@ module.exports = class Product {
   }
 
   save() {
-    
+    return db.execute("insert into products (title,price,description,imageUrl) values (?,?,?,?)",
+      [this.title, this.price, this.description, this.imageUrl]);
+
   }
 
   static deleteById(id) {
-  
+
   }
 
   static fetchAll() {
     return db.execute("select * from products")
-   
+
   }
   static findById(id, cb) {
-   
+
   }
 };
