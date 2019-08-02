d79a55cc2ddef408764743530d37f708c8e384b6
unknown <jupiter85838@yahoo.com>
2019-08-02 15:40:25 -0400

    get data from db using sequelize ORM

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,14 +2,13 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
+  Product.findAll().then(products=>{
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-
-  }).catch(err => {
+  }).catch(err=>{
     console.log(err)
   })
 };
@@ -33,13 +32,12 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
+  Product.findAll().then(products => {
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
-
   }).catch(err => {
     console.log(err)
   })
