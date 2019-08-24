63e5752d8df8e07fc57cc100dbe60d445f9b8ccd
unknown <jupiter85838@yahoo.com>
2019-08-24 14:45:54 -0400

    adding a user

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 
 const connection_string = require('./util/decrept')
 const errorController = require('./controllers/error');
-//const User = require('./models/user')
+const User = require('./models/user')
 
 
 const app = express();
@@ -22,18 +22,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //register a middleware to attach the user to incomming requests
-// app.use((req, res, next) => {
-//     return User.findById("5d599643161f412d788aac10").then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
+app.use((req, res, next) => {
+    return User.findById("5d618549ff19836f1cd09cbe").then(user => {
+        req.user = user;
+        next();
 
-//     }).catch(err => {
-//         console.log(err)
+    }).catch(err => {
+        console.log(err)
 
-//     })
+    })
 
 
-// })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -41,6 +41,13 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect(connection_string).then(result => {
+    User.findOne().then(user => {
+        if (!user) {
+            const user = new User({ name: "user1", email: "a@a.com", cart: { items: [] } })
+            user.save()
+        }
+    })
+
     app.listen(3000)
 }).catch(err => {
     console.log(err)
