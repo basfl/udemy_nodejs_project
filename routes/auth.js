a4e05e92625c4c3eaba71e1b130b190a471f8557
unknown <jupiter85838@yahoo.com>
2019-08-31 16:26:18 -0400

    protecting routes by adding middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const authController = require('../controllers/auth');
-const isAuth = require('../middleware/is-auth')
 const router = express.Router();
-router.use(isAuth)
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
