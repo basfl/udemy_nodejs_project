e24590125258ca1f1c3c8a5e7059cf724a46e86c
unknown <jupiter85838@yahoo.com>
2019-09-15 12:41:07 -0400

    add custom validation to forbid test@test.com

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,15 @@ router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 // adding validation middleware i.e check
-router.post('/signup', check('email').isEmail().withMessage('please enter a valid email !'), authController.postSignup);
+router.post('/signup', check('email').isEmail()
+    .withMessage('please enter a valid email !')
+    .custom((value, { req }) => {
+        if (value === "test@test.com") {
+            throw new Error("This Email is Forbidden!!");
+        }
+        return true;
+    }),
+    authController.postSignup);
 router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
